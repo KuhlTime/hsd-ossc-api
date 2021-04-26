88358dbbfbb6c77d05f78034281f3269c7a07db6
André Kuhlmann <akuhltime@gmail.com>
2021-04-26 18:09:11 +0200

    Reformat index.ts

--- src/models/index.ts
+++ src/models/index.ts
@@ -2,9 +2,9 @@ import Class from './Class'
 import Exam from './Exam'
 import Module from './Module'
 import ModuleExtract from './ModuleExtract'
+import Score from './Score'
 import Semester from './Semester'
 import Student from './Student'
 import WorkExperience from './WorkExperience'
-import Score from './Score'
 
 export { Class, Exam, Module, ModuleExtract, Semester, Student, WorkExperience, Score }
