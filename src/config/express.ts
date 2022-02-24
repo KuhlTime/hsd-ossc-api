054e467e972760f6ef96633789b44f49d8f56552
André Kuhlmann <akuhltime@gmail.com>
2022-02-24 10:31:50 +0100

    Make Sentry Optional

--- src/config/express.ts
+++ src/config/express.ts
@@ -4,7 +4,7 @@ import morgan from 'morgan'
 import * as Sentry from '@sentry/node'
 import * as Tracing from '@sentry/tracing'
 
-import { morganFormat } from './env'
+import { morganFormat, sentryDsn } from './env'
 
 // load router
 import osscRouter from '../routes/ossc'
@@ -13,25 +13,29 @@ import dataRouter from '../routes/data'
 // create server
 const app = express()
 
-Sentry.init({
-	dsn: 'https://ee9006d7bee240f6b5da0669aabf2819@o459771.ingest.sentry.io/5461979',
-	integrations: [
-		// enable HTTP calls tracing
-		new Sentry.Integrations.Http({ tracing: true }),
-		// enable Express.js middleware tracing
-		new Tracing.Integrations.Express({ app })
-	],
+if (sentryDsn) {
+	Sentry.init({
+		dsn: sentryDsn,
+		integrations: [
+			// enable HTTP calls tracing
+			new Sentry.Integrations.Http({ tracing: true }),
+			// enable Express.js middleware tracing
+			new Tracing.Integrations.Express({ app })
+		],
 
-	// We recommend adjusting this value in production, or using tracesSampler
-	// for finer control
-	tracesSampleRate: 1.0
-})
+		// We recommend adjusting this value in production, or using tracesSampler
+		// for finer control
+		tracesSampleRate: 1.0
+	})
 
-// RequestHandler creates a separate execution context using domains, so that every
-// transaction/span/breadcrumb is attached to its own Hub instance
-app.use(Sentry.Handlers.requestHandler())
-// TracingHandler creates a trace for every incoming request
-app.use(Sentry.Handlers.tracingHandler())
+	// RequestHandler creates a separate execution context using domains, so that every
+	// transaction/span/breadcrumb is attached to its own Hub instance
+	app.use(Sentry.Handlers.requestHandler())
+	// TracingHandler creates a trace for every incoming request
+	app.use(Sentry.Handlers.tracingHandler())
+} else {
+	console.log('Sentry DSN not set, skipping Sentry initialization')
+}
 
 // setup middlewares
 app.use(express.json())
