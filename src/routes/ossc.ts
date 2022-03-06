01f89fd1f3266b4b178f1fda903f83cb1d3c90bb
André Kuhlmann <akuhltime@gmail.com>
2022-03-06 22:27:26 +0100

    Remove console.log

--- src/routes/ossc.ts
+++ src/routes/ossc.ts
@@ -9,8 +9,6 @@ const router = Router()
 
 // set routes
 router.get('/', authGuard, (req: any, res: Response) => {
-	console.log(req.user)
-
 	OsscSession.requestGrades(req.user.name, req.user.pass)
 		.then(data => res.send({ type: 'success', data: data }))
 		.catch(err => res.status(500).send({ type: 'error', message: err.message }))
