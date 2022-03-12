4be31fef22157673f6f2778d8d39d84e45086f2e
André Kuhlmann <akuhltime@gmail.com>
2022-03-12 17:50:28 +0100

    Add PlainTextAgents

---
+++ src/utilities/PlainTextAgents.ts
@@ -0,0 +1,15 @@
+// inspired by https://github.com/chubin/wttr.in/blob/85d7d029a20a2ed659f7041cb76fa8848cce5825/cmd/srv.go#L21
+var plainTextAgents = [
+	'curl',
+	'httpie',
+	'lwp-request',
+	'wget',
+	'python-requests',
+	'openbsd ftp',
+	'powershell',
+	'fetch',
+	'aiohttp',
+	'http_get'
+]
+
+export default plainTextAgents
